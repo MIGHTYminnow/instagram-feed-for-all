e1b378ef18a2bc593e20b580bc35a8b0836457cb
Aaron Barraza <aaron@mightyminnow.com>
2019-10-02 23:41:53 -0700

    [TAG] Version 1.0.0

---
+++ instagram-feed-for-all.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Instagram Feed for All
+ * Plugin URI: https://github.com/MIGHTYminnow/instagram-feed-for-all
+ * Description: Makes Instagram Feed accessible.
+ * Version: 1.0.0-alpha
+ * Author: MIGHTYminnow
+ * Author URI: https://mightyminnow.com
+ */
+
+/**
+ * Enqueue child theme styles and scripts.
+ */
+if ( defined( 'SBIVER' ) ) {
+	if ( ! function_exists( 'ifa_enqueue_assets' ) ) {
+		add_action( 'wp_enqueue_scripts', 'ifa_enqueue_assets' );
+		function ifa_enqueue_assets() {
+			wp_enqueue_script( 'instagram-feed-for-all', plugin_dir_url( __FILE__ ) . 'instagram-feed-for-all.js', array( 'jquery' ), '1.0.0-alpha', true );
+		}
+	}
+}
